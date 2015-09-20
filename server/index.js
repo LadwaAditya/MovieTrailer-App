72ef1715863f03bab2c7f5613f0399143e6c1665
Aditya Ladwa <ladwa.aditya@gmail.com>
2015-09-20 22:37:53 +0530

    Wrote basic index.js file

--- server/index.js
+++ server/index.js
@@ -3,3 +3,26 @@ var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var _ = require('lodash');
+
+//Create express app
+var app =express();
+
+//Add middleware necessary for REST api
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+//Add CORS support
+app.use(function(req,res,next){
+	res.header('Access-Control-Allow-Origin','*');
+	res.header('Access-Control-Allow-Methods','GET,POST,PUT,DELETE');
+	res.header('Access-Control-Allow-Headers','Content-Type');
+	next();
+});
+
+//Connect to MongoDb database
+mongoose.connect('mongodb://localhost/movieTrailerApp');
+mongoose.connection.once('open',function(){
+	console.log('Listning on port 3000');
+	app.listen(3000);
+});
